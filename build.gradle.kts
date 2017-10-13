1522a4620aa3d661f3c4f834f0f835b06f84cc58
Sebastien Deleuze <sdeleuze@pivotal.io>
2017-10-13 10:02:09 +0200

    Upgrade to Gradle 4.3-rc-1 and tune Gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,24 +4,8 @@ plugins {
 	id("org.jetbrains.kotlin.jvm") version "1.1.51"
 	id("org.jetbrains.kotlin.plugin.spring") version "1.1.51"
 	id("io.spring.dependency-management") version "1.0.3.RELEASE"
-}
-
-buildscript {
-	repositories {
-		mavenCentral()
-		jcenter()
-		maven("https://repo.spring.io/milestone")
-	}
-
-	dependencies {
-		classpath("org.springframework.boot:spring-boot-gradle-plugin:2.0.0.M5")
-		classpath("org.junit.platform:junit-platform-gradle-plugin:1.0.0")
-	}
-}
-
-apply {
-	plugin("org.springframework.boot")
-	plugin("org.junit.platform.gradle.plugin")
+    id("org.springframework.boot") version "2.0.0.M5"
+    id("org.junit.platform.gradle.plugin") version "1.0.0"
 
 }
 
