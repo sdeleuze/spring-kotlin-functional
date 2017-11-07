2bcd3de63a87fd4ac0d23088ab83333579920ad4
sdeleuze <sdeleuze@pivotal.io>
2017-11-07 14:47:13 +0100

    Use runApplication top level function

--- src/main/kotlin/functional/Application.kt
+++ src/main/kotlin/functional/Application.kt
@@ -1,11 +1,11 @@
 package functional
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
-	SpringApplication.run(Application::class.java, *args)
+	runApplication<Application>(*args)
 }
