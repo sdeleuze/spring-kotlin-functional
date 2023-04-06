5ee36e1ed72f042a3fcd9be81c39851ad28d9f79
Sébastien Deleuze <sdeleuze@vmware.com>
2023-04-06 16:09:09 +0200

    Polishing

--- src/test/kotlin/functional/IntegrationTests.kt
+++ src/test/kotlin/functional/IntegrationTests.kt
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType.*
-import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.expectBody
 import org.springframework.test.web.reactive.server.expectBodyList
